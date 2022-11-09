609b51f5a95ecb87b0ec1338d2957a18d2b1597a
Ines <ines.cvek16@gmail.com>
2022-11-09 11:09:20 +0100

    Added basic styles

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const path = require("path");
 const methodOverride = require("method-override");
 const mongoose = require("mongoose");
+const ejsMate = require("ejs-mate");
 const Campground = require("./models/campground");
 mongoose.connect("mongodb://localhost:27017/yelp-camp", {
     useNewUrlParser: true, 
@@ -16,6 +17,7 @@ db.once("open", () => {
     console.log("Database connected");
 });
 
+app.engine("ejs", ejsMate)
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
