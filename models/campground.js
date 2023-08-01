f7ffc87c87796fb1595ff47f400304ebbb51f9ad
Ines <ines.cvek16@gmail.com>
2023-08-01 17:00:30 +0200

    add cluster map to index

--- models/campground.js
+++ models/campground.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const Review = require("./review");
+const opts = {toJSON: {virtuals:true}};
 
 const imageSchema = new Schema({
     url: String,
@@ -23,7 +24,12 @@ const campgroundSchema = new Schema({
     location: String,
     reviews: [{type: Schema.Types.ObjectId, ref: "Review"}],
     author: {type:Schema.Types.ObjectId, ref: "User"}
-});
+}, opts);
+
+campgroundSchema.virtual("properties.popUpMarkup").get(function () {
+    return `<a href="/campgrounds/${this._id}">${this.title}</a>
+    <p>${this.description.substring(0,20)}...</p>`
+})
 
 campgroundSchema.post("findOneAndDelete", async function(campground) {
     if(campground.reviews.length) {
